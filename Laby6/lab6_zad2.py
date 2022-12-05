59601195baba799eab6622847cdadbf0feae6d94
Bartosz Kloc <atlf75800@gmail.com>
2022-12-05 12:10:56 +0100

    lab6_zad2

--- Laby6/lab6_zad2.py
+++ Laby6/lab6_zad2.py
@@ -12,11 +12,14 @@ from PIL import Image
 viewer = [0.0, 0.0, 10.0]
 
 theta = 0.0
+phi = 0.0
 pix2angle = 1.0
-
+piy2angle = 1.0
 left_mouse_button_pressed = 0
 mouse_x_pos_old = 0
+mouse_y_pos_old = 0
 delta_x = 0
+delta_y = 0
 
 mat_ambient = [1.0, 1.0, 1.0, 1.0]
 mat_diffuse = [1.0, 1.0, 1.0, 1.0]
@@ -76,6 +79,7 @@ def shutdown():
 
 def render(time):
     global theta
+    global phi
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
@@ -83,9 +87,13 @@ def render(time):
     gluLookAt(viewer[0], viewer[1], viewer[2],
               0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
 
+    R = 10
+
     if left_mouse_button_pressed:
         theta += delta_x * pix2angle
+        phi += delta_y * pix2angle
 
+    glRotatef(phi, 1.0, 0.0, 0.0)
     glRotatef(theta, 0.0, 1.0, 0.0)
 
     # podstawa:
@@ -173,14 +181,21 @@ def mouse_motion_callback(window, x_pos, y_pos):
     delta_x = x_pos - mouse_x_pos_old
     mouse_x_pos_old = x_pos
 
+    global delta_y
+    global mouse_y_pos_old
+
+    delta_y = y_pos - mouse_y_pos_old
+    mouse_y_pos_old = y_pos
+
 
 def mouse_button_callback(window, button, action, mods):
     global left_mouse_button_pressed
 
-    if button == GLFW_MOUSE_BUTTON_LEFT and action == GLFW_PRESS:
-        left_mouse_button_pressed = 1
-    else:
-        left_mouse_button_pressed = 0
+    if button == GLFW_MOUSE_BUTTON_LEFT:
+        if action == GLFW_PRESS:
+            left_mouse_button_pressed = 1
+        else:
+            left_mouse_button_pressed = 0
 
 
 def main():
